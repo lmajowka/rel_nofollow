6f0b2a684ca0bb0ad4eba335b4e1b342692fba65
Leo <leo.majowka@gmail.com>
2015-07-28 00:14:22 +0300

    fix method

--- lib/rel_nofollow/string.rb
+++ lib/rel_nofollow/string.rb
@@ -2,7 +2,7 @@ class String
   def add_nofollow
     self.scan(/(\<a href=["'].*?["']\>.*?\<\/a\>)/).flatten.each do |link|
       link.match(/(\<a href=["'](.*?)["']\>(.*?)\<\/a\>)/)
-      html.gsub!(link, "<a href='#{$2}' rel='nofollow'>#{$3}</a>" )
+      self.gsub!(link, "<a href='#{$2}' rel='nofollow'>#{$3}</a>" )
     end
   end
 end
