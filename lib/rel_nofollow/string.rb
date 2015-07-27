413620a23b838f582516d9a08ef392549400beb7
Leo <leo.majowka@gmail.com>
2015-07-28 00:43:56 +0300

    fix #add_nofollow

--- lib/rel_nofollow/string.rb
+++ lib/rel_nofollow/string.rb
@@ -1,7 +1,7 @@
 class String
   def add_nofollow
     self.gsub(/<a [^>]+>/) do |link|
-      unless aref.include?('rel="nofollow"')
+      unless link.include?('rel="nofollow"')
         link = link[0..-2] + ' rel="nofollow">'
       end
       link
--- lib/rel_nofollow/version.rb
+++ lib/rel_nofollow/version.rb
@@ -1,3 +1,3 @@
 module RelNofollow
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
